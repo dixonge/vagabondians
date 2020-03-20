71c7ed305f7a4ff150a3996e6f38f173a7ede177
Glenn Dixon <donblanco@posteo.net>
2020-03-20 12:31:50 -0600

    changed syntax per error

--- postcss.config.js
+++ postcss.config.js
@@ -9,12 +9,10 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
 })
 
-module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-    ...process.env.NODE_ENV === 'production'
-      ? [purgecss]
-      : []
-  ]
-}
+export const plugins = [
+  require('tailwindcss'),
+  require('autoprefixer'),
+  ...process.env.NODE_ENV === 'production'
+    ? [purgecss]
+    : []
+]
