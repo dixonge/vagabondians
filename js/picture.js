f468e9746088859c20432851168d0ae943317eb9
Glenn Dixon <donblanco@posteo.net>
2020-08-24 06:23:51 -0700

    add dpr_auto to cloudinary images

--- js/picture.js
+++ js/picture.js
@@ -1,10 +1,10 @@
 
 module.exports = (url, alt = "Missing alt text") => {
     return `<picture>
-    <source srcset="https://res.cloudinary.com/donblanco/image/upload/f_auto,q_auto,w_320/Vagabondians/${url}" media="(max-width: 320px)">
-    <source srcset="https://res.cloudinary.com/donblanco/image/upload/f_auto,q_auto,w_375/Vagabondians/${url}" media="(max-width: 375px)">
-    <source srcset="https://res.cloudinary.com/donblanco/image/upload/f_auto,q_auto,w_414/Vagabondians/${url}" media="(max-width: 414px)">
-    <source srcset="https://res.cloudinary.com/donblanco/image/upload/f_auto,q_auto,w_756/Vagabondians/${url}" media="(min-width: 755px)">
-    <img src="https://res.cloudinary.com/donblanco/image/upload/f_auto,q_auto,w_756/Vagabondians/${url}" alt="${alt}" />
+    <source srcset="https://res.cloudinary.com/donblanco/image/upload/dpr_auto,f_auto,q_auto,w_320/Vagabondians/${url}" media="(max-width: 320px)">
+    <source srcset="https://res.cloudinary.com/donblanco/image/upload/dpr_auto,f_auto,q_auto,w_375/Vagabondians/${url}" media="(max-width: 375px)">
+    <source srcset="https://res.cloudinary.com/donblanco/image/upload/dpr_auto,f_auto,q_auto,w_414/Vagabondians/${url}" media="(max-width: 414px)">
+    <source srcset="https://res.cloudinary.com/donblanco/image/upload/dpr_auto,f_auto,q_auto,w_756/Vagabondians/${url}" media="(min-width: 755px)">
+    <img src="https://res.cloudinary.com/donblanco/image/upload/dpr_auto,f_auto,q_auto,w_756/Vagabondians/${url}" alt="${alt}" />
   </picture>`;
   };
