93614df4c55e5e798a1c1816d9ff214847e2de43
Glenn Dixon <donblanco@posteo.net>
2020-08-24 06:45:34 -0700

    fix css on images

--- js/picture.js
+++ js/picture.js
@@ -5,6 +5,6 @@ module.exports = (url, alt = "Missing alt text") => {
     <source srcset="https://res.cloudinary.com/donblanco/image/upload/dpr_auto,f_auto,q_auto,w_375/Vagabondians/${url}" media="(max-width: 375px)">
     <source srcset="https://res.cloudinary.com/donblanco/image/upload/dpr_auto,f_auto,q_auto,w_414/Vagabondians/${url}" media="(max-width: 414px)">
     <source srcset="https://res.cloudinary.com/donblanco/image/upload/dpr_auto,f_auto,q_auto,w_756/Vagabondians/${url}" media="(min-width: 755px)">
-    <img src="https://res.cloudinary.com/donblanco/image/upload/dpr_auto,f_auto,q_auto,w_756/Vagabondians/${url}" alt="${alt}" />
+    <img src="https://res.cloudinary.com/donblanco/image/upload/dpr_auto,f_auto,q_auto,w_756/Vagabondians/${url}" alt="${alt}" class="mt-1 ml-auto mr-auto" />
   </picture>`;
   };
