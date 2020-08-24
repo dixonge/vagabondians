aa539ba696a657c8973294906906a55e9aa52da7
Glenn Dixon <donblanco@posteo.net>
2020-08-24 14:06:58 -0700

    alternate srcset syntax

--- js/picture.js
+++ js/picture.js
@@ -1,10 +1,6 @@
 
 module.exports = (url, alt = "Missing alt text") => {
-    return `<picture>
-    <source srcset="https://res.cloudinary.com/donblanco/image/upload/dpr_auto,f_auto,q_auto,w_320/Vagabondians/${url}" media="(max-width: 320px)">
-    <source srcset="https://res.cloudinary.com/donblanco/image/upload/dpr_auto,f_auto,q_auto,w_375/Vagabondians/${url}" media="(max-width: 375px)">
-    <source srcset="https://res.cloudinary.com/donblanco/image/upload/dpr_auto,f_auto,q_auto,w_414/Vagabondians/${url}" media="(max-width: 414px)">
-    <source srcset="https://res.cloudinary.com/donblanco/image/upload/dpr_auto,f_auto,q_auto,w_756/Vagabondians/${url}" media="(min-width: 755px)">
-    <img src="https://res.cloudinary.com/donblanco/image/upload/dpr_auto,f_auto,q_auto,w_756/Vagabondians/${url}" alt="${alt}" />
-  </picture>`;
+    return `
+    <img src="https://res.cloudinary.com/donblanco/image/upload/dpr_auto,f_auto,q_auto,w_756/Vagabondians/${url}" alt="${alt}" srcset="https://res.cloudinary.com/donblanco/image/upload/dpr_auto,f_auto,q_auto,w_320/Vagabondians/${url} 320w, https://res.cloudinary.com/donblanco/image/upload/dpr_auto,f_auto,q_auto,w_375/Vagabondians/${url} 375w, https://res.cloudinary.com/donblanco/image/upload/dpr_auto,f_auto,q_auto,w_414/Vagabondians/${url} 414w, https://res.cloudinary.com/donblanco/image/upload/dpr_auto,f_auto,q_auto,w_756/Vagabondians/${url} 756w" />
+  `;
   };
