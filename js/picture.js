67b6e029b9f4bdef344ce3606dc51945afe17c48
Glenn Dixon <donblanco@posteo.net>
2020-08-26 07:07:56 -0700

    add lazy load

--- js/picture.js
+++ js/picture.js
@@ -1,6 +1,6 @@
 
 module.exports = (url, alt = "Missing alt text") => {
     return `
-    <img src="https://res.cloudinary.com/donblanco/image/upload/dpr_auto,f_auto,q_auto/Vagabondians/${url}" alt="${alt}" srcset="https://res.cloudinary.com/donblanco/image/upload/dpr_auto,f_auto,q_auto,w_320/Vagabondians/${url} 320w, https://res.cloudinary.com/donblanco/image/upload/dpr_auto,f_auto,q_auto,w_375/Vagabondians/${url} 375w, https://res.cloudinary.com/donblanco/image/upload/dpr_auto,f_auto,q_auto,w_414/Vagabondians/${url} 414w, https://res.cloudinary.com/donblanco/image/upload/dpr_auto,f_auto,q_auto,w_756/Vagabondians/${url} 756w, https://res.cloudinary.com/donblanco/image/upload/dpr_auto,f_auto,q_auto,w_960/Vagabondians/${url} 960w" />
+    <img src="https://res.cloudinary.com/donblanco/image/upload/dpr_auto,f_auto,q_auto/Vagabondians/${url}" alt="${alt}" loading="lazy" srcset="https://res.cloudinary.com/donblanco/image/upload/dpr_auto,f_auto,q_auto,w_320/Vagabondians/${url} 320w, https://res.cloudinary.com/donblanco/image/upload/dpr_auto,f_auto,q_auto,w_375/Vagabondians/${url} 375w, https://res.cloudinary.com/donblanco/image/upload/dpr_auto,f_auto,q_auto,w_414/Vagabondians/${url} 414w, https://res.cloudinary.com/donblanco/image/upload/dpr_auto,f_auto,q_auto,w_756/Vagabondians/${url} 756w, https://res.cloudinary.com/donblanco/image/upload/dpr_auto,f_auto,q_auto,w_960/Vagabondians/${url} 960w" />
   `;
   };
